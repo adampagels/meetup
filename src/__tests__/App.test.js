b91da84144d4d3e17b93d7999af969c160e38b6c
Adam Pagels <pagels@hotmail.ca>
2019-11-10 10:14:28 -0500

    Add initial test for EventList

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import App from '../App';
+import EventList from '../EventList';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App /> component', () => {
+  test('render list of events', () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
 });
