29f0cc5aeef597b032f52551c7d46e01359c22a5
Adam Pagels <pagels@hotmail.ca>
2019-11-10 20:40:43 -0500

    Add input field

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
     render() {
         return (
-            <div>
-
+            <div className="CitySearch">
+                <input
+                    type="text"
+                    className="city"
+                />
             </div>
         );
     }
