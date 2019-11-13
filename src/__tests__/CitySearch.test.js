6e8ba750f08e765c9fcd4a04de4679793c21de90
Adam Pagels <pagels@hotmail.ca>
2019-11-12 22:23:55 -0500

    Refactor tests with beforeAll function

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -2,33 +2,33 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
 
+let CitySearchWrapper;
+beforeAll(() => {
+    CitySearchWrapper = shallow(<CitySearch />);
+});
+
 describe('<CitySearch /> component', () => {
     test('render text input', () => {
-        const CitySearchWrapper = shallow(<CitySearch />);
         expect(CitySearchWrapper.find('.city')).toHaveLength(1);
     });
 });
 
 test('render list of suggestions', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
 });
 
 test('render text input correctly', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     const query = CitySearchWrapper.state('query');
     expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
 });
 
 test('change state when text input changes', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     const eventObject = { target: { value: 'Berlin' } };
     CitySearchWrapper.find('.city').simulate('change', eventObject);
     expect(CitySearchWrapper.state('query')).toBe('Berlin');
 });
 
 test('render list of suggestions correctly', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     const suggestions = CitySearchWrapper.state('suggestions');
     expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length);
     for (let i = 0; i < suggestions.length; i += 1) {
@@ -37,7 +37,6 @@ test('render list of suggestions correctly', () => {
 });
 
 test('click on suggestion should change query state', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     CitySearchWrapper.setState({
         suggestions: [
             {
