2a10b3116d2134f15b703e9b67f322756de5355a
Adam Pagels <pagels@hotmail.ca>
2019-11-13 21:33:43 -0500

    Add div container

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NumberOfEvents extends Component {
+    state = {
+    }
+    render() {
+        return (
+            <div className="numberOfEvents">
+                <label>Number of Events: </label>
+            </div>
+        );
+    }
+}
+
+export default NumberOfEvents;
