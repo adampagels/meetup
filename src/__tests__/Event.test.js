ae1fea8c5313b2fa2ee00ca8388fabed36847d90
Adam Pagels <pagels@hotmail.ca>
2019-11-13 19:56:04 -0500

    Test for event-info's children render

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -12,15 +12,22 @@ describe('<App /> component', () => {
         expect(EventWrapper.find('.event')).toHaveLength(1);
     });
 
-    test('render event info', () => {
-        expect(EventWrapper.find('.event-info')).toHaveLength(1);
+    test('render event-overview', () => {
+        expect(EventWrapper.find('.event-overview')).toHaveLength(1);
     });
 
     test('render event-name', () => {
         expect(EventWrapper.find('.event-name')).toHaveLength(1);
     });
 
-    test('render children of event-info', () => {
-        expect(EventWrapper.find('.event-info').children()).toHaveLength(3);
+    test('render children of event-overview', () => {
+        expect(EventWrapper.find('.event-overview').children()).toHaveLength(3);
+    });
+
+    test('render children of event-info ', () => {
+        EventWrapper.setState({
+            showDetails: true
+        });
+        expect(EventWrapper.find('.event-info')).toHaveLength(1);
     });
 });
