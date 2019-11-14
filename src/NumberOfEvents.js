147d7deeecdca7615c471b913adaff933554127b
Adam Pagels <pagels@hotmail.ca>
2019-11-13 21:36:42 -0500

    Add state

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
     state = {
+        numberOfEvents: 10
     }
+
     render() {
         return (
-            <div className="numberOfEvents">
+            <div className="number-of-events">
                 <label>Number of Events: </label>
+                <input
+                    type="text"
+                    class="event-number"
+                    value={this.state.numberOfEvents}
+                    onChange={this.handleInputChanged}
+                />
             </div>
         );
     }
 }
+
 
 export default NumberOfEvents;
