cc9857f6474b797095c53ac690f77fa90e6dcb94
Adam Pagels <pagels@hotmail.ca>
2019-11-13 21:56:44 -0500

    Test for state change

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -16,4 +16,10 @@ describe('<NumberOfEvents /> component', () => {
         const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
         expect(NumberOfEventsWrapper.find('#event-number').prop('value')).toBe(numberOfEvents);
     });
+
+    test('state change on input change', () => {
+        const eventObject = { target: { value: 32 } };
+        NumberOfEventsWrapper.find('#event-number').simulate('change', eventObject);
+        expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
+    });
 });
