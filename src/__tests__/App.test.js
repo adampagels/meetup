35bc9d466908766b01f09275a91496cddb4065e9
Adam Pagels <pagels@hotmail.ca>
2019-11-14 21:39:28 -0500

    Test for App integration

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
@@ -22,5 +22,16 @@ describe('<App /> component', () => {
 
   test('render NumberOfEvents', () => {
     expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+  });
+
+  describe('<App /> integration', () => {
+    test('get list of events after user selects a city', async () => {
+      const AppWrapper = mount(<App />);
+      AppWrapper.instance().updateEvents = jest.fn();
+      AppWrapper.instance().forceUpdate();
+      const CitySearchWrapper = AppWrapper.find(CitySearch);
+      CitySearchWrapper.instance().handleItemClicked('value');
+      expect(AppWrapper.instance().updateEvents).toHaveBeenCalledTimes(1);
+    });
   });
 });
