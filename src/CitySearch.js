045b18b228d2ec04b782a6c88ca88a139a7d8eb4
Adam Pagels <pagels@hotmail.ca>
2019-11-14 21:07:02 -0500

    Import getSuggestions function

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { getSuggestions } from './api';
+
 class CitySearch extends Component {
     state = {
         query: '',
@@ -9,6 +11,7 @@ class CitySearch extends Component {
     handleInputChanged = event => {
         const value = event.target.value;
         this.setState({ query: value });
+        getSuggestions(value).then(suggestions => this.setState({ suggestions }));
     };
 
     handleItemClicked = value => {
