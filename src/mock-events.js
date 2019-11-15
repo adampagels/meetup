b0fddc247b8993f0c83f5dd4434c17f21996d28b
Adam Pagels <pagels@hotmail.ca>
2019-11-14 21:16:58 -0500

    First Commit

---
+++ src/mock-events.js
@@ -0,0 +1,15 @@
+const mockEvents = {
+    city: {
+        id: 1007700,
+        city: 'München',
+        lat: 48.14,
+        lon: 11.58,
+        state: '',
+        country: 'de',
+        zip: 'meetup3',
+        member_count: 1257
+    },
+    events: []
+};
+
+export { mockEvents }
