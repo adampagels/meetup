87cbd516d3778819df6b64a0e5fe365f5253f1dc
Adam Pagels <pagels@hotmail.ca>
2019-11-15 22:01:51 -0500

    Update updateEvents

--- src/App.js
+++ src/App.js
@@ -15,30 +15,34 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.updateEvents(null, null)
+    getEvents().then(response => this.setState({ events: response }));
   }
 
   updateEvents = (lat, lon, page) => {
     if (lat && lon) {
-      getEvents(lat, lon, this.state.page).then(events =>
-        this.setState({ events, lat, lon })
+      getEvents(lat, lon, this.state.page).then(response =>
+        this.setState({ events: response, lat, lon })
       );
     } else if (page) {
-      getEvents(this.state.lat, this.state.lon, page).then(events =>
-        this.setState({ events, page })
+      getEvents(this.state.lat, this.state.lon, page).then(response =>
+        this.setState({ events: response, page })
       );
     } else {
-      getEvents(this.state.lat, this.state.lon, this.state.page).then(events =>
-        this.setState({ events })
+      getEvents(this.state.lat, this.state.lon, this.state.page).then(
+        response => this.setState({ events: response })
       );
     }
-  }
+  };
 
   render() {
     return (
-      <div className="App" >
+      <div className="App">
         <CitySearch updateEvents={this.updateEvents} />
-        <NumberOfEvents />
+        <NumberOfEvents
+          updateEvents={this.updateEvents}
+          numberOfEvents={this.state.events.length}
+          lat={this.state.lat}
+          lon={this.state.lon} />
         <EventList events={this.state.events} />
       </div>
     );
