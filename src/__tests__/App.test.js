4adbe51ddba85464a4d9ee998033c90f0ed5babf
Adam Pagels <pagels@hotmail.ca>
2019-11-15 22:06:21 -0500

    Add integration tests for NumberOfEvents

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -38,11 +38,30 @@ describe('<App /> integration', () => {
     AppWrapper.unmount();
   });
 
+  test('update list of events after user changes number of events', () => {
+    const AppWrapper = mount(<App />);
+    AppWrapper.instance().updateEvents = jest.fn();
+    AppWrapper.instance().forceUpdate();
+    const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+    NumberOfEventsWrapper.instance().handleChange({ target: { value: 1 } });
+    expect(AppWrapper.instance().updateEvents).toHaveBeenCalledTimes(1);
+    expect(AppWrapper.instance().updateEvents).toHaveBeenCalledWith(null, null, 1);
+    AppWrapper.unmount();
+  });
+
   test('change state after get list of events', async () => {
     const AppWrapper = shallow(<App />);
     AppWrapper.instance().updateEvents(1.1, 1.2);
     await AppWrapper.update();
     expect(AppWrapper.state('events')).toEqual(mockEvents.events);
+    AppWrapper.unmount();
   });
+
+  test('render correct list of events', () => {
+    const AppWrapper = mount(<App />);
+    AppWrapper.setState({ events: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }] });
+    expect(AppWrapper.find('.Event')).toHaveLength(4);
+    AppWrapper.unmount();
+  })
 
 });
