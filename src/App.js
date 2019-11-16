976078bc633fdb1e08bfaa65288f1232b7223032
Adam Pagels <pagels@hotmail.ca>
2019-11-15 20:32:43 -0500

    Add function to render an eventlist based on whether or whether not a location is specified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
@@ -7,24 +6,40 @@ import NumberOfEvents from './NumberOfEvents';
 import { getEvents } from './api';
 
 class App extends Component {
+
   state = {
     events: [],
-    defaultCity: '',
-    numberOfEvents: '',
-    lat: '',
-    lon: ''
+    page: null,
+    lat: null,
+    lon: null,
   }
 
-  updateEvents = (lat, lon) => {
-    getEvents(lat, lon).then(events => this.setState({ events }));
+  componentDidMount() {
+    this.updateEvents(null, null)
+  }
+
+  updateEvents = (lat, lon, page) => {
+    if (lat && lon) {
+      getEvents(lat, lon, this.state.page).then(events =>
+        this.setState({ events, lat, lon })
+      );
+    } else if (page) {
+      getEvents(this.state.lat, this.state.lon, page).then(events =>
+        this.setState({ events, page })
+      );
+    } else {
+      getEvents(this.state.lat, this.state.lon, this.state.page).then(events =>
+        this.setState({ events })
+      );
+    }
   }
 
   render() {
     return (
-      <div className="App">
+      <div className="App" >
         <CitySearch updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
         <NumberOfEvents />
+        <EventList events={this.state.events} />
       </div>
     );
   }
